c8efb14b8c1375b0843cff040686a872ddf1bb37
Daniel Caldas <daniel.caldasgoncalves@gmail.com>
2023-11-25 18:57:54 -0300

    colocando comando build

--- src/index.js
+++ src/index.js
@@ -10,12 +10,4 @@ app.get("/", (req, res) => {
     res.json({mensagem: "Olá"})
 })
 
-const separa = new Date().toLocaleDateString().split("/")
-var reverse = separa.reverse()
-var join = reverse.join("/")
-
-var time = new Date().toLocaleTimeString()
-var every = join + ' ' + time
-const date = new Date().toISOString();
-
-app.listen(3333, () => console.log("server is running"))
+app.listen(3333 || process.env.PORT, () => console.log("server is running"))
